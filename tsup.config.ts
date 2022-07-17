5f1d11e5fb0d8afa1301ab9ea2501ad5f00529f0
didinele <didinele.dev@gmail.com>
2022-07-17 16:19:57 +0300

    chore: initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,32 @@
+import { relative, resolve } from 'node:path';
+import { defineConfig, type Options } from 'tsup';
+
+type ConfigOptions = Pick<
+	Options,
+	'globalName' | 'minify' | 'entry' | 'format' | 'target' | 'sourcemap' | 'noExternal' | 'esbuildOptions'
+>;
+
+export const createTsupConfig = ({
+	globalName,
+	format = ['esm', 'cjs'],
+	target = 'es2021',
+	sourcemap = true,
+	minify = false,
+	entry = ['src/index.ts'],
+	noExternal,
+	esbuildOptions,
+}: ConfigOptions = {}) =>
+	defineConfig({
+		clean: true,
+		entry,
+		format,
+		minify,
+		skipNodeModulesBundle: true,
+		sourcemap,
+		target,
+		tsconfig: relative(__dirname, resolve(process.cwd(), 'tsconfig.json')),
+		keepNames: true,
+		globalName,
+		noExternal,
+		esbuildOptions,
+	});
